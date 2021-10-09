eb57b637ce8ecf8806082bcf710b87bc15df5c35
Axel Anaya <axel.ana25@gmail.com>
2021-10-09 18:00:57 -0500

    Created UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,7 @@
+import { User } from "../entities/User";
+import UserRepository from "../repositories/UserRepository";
+
+import { Request, Response } from "express";
+import { Repository } from "typeorm";
+
+export const StudentController = (userRepository: UserRepository) => {};
