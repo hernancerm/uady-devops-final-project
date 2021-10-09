664d0cbdc463d6a1fdac7ac2bf76b4002314417b
Hernán Cervera <hjcerveram@hotmail.com>
2021-10-09 11:48:59 -0500

    Add more inserts in populate mysql script

--- mysql/uady_sicei_populate.sql
+++ mysql/uady_sicei_populate.sql
@@ -1,9 +1,11 @@
 USE uady_sicei;
 
 # Insert students
-INSERT INTO student (first_names, last_names,  birth_date, sex, enrollment_date)
-  VALUES ("Juan", "García", "2017-08-12", "M", "2021-04-02");
-INSERT INTO student (first_names, last_names,  birth_date, sex, enrollment_date)
-  VALUES ("Sofía", "Palmira", "2017-08-12", "F", "2021-04-02");
-INSERT INTO student (first_names, last_names,  birth_date, sex, enrollment_date)
-  VALUES ("Pedro", "Escalante", "2017-08-12", "M", "2021-04-02");
+INSERT INTO student (first_names, last_names, birth_date, sex, enrollment_date)
+  VALUES ("Juan", "García", "1997-03-01", "M", "2017-08-12");
+INSERT INTO student (first_names, last_names, birth_date, sex, enrollment_date)
+  VALUES ("Sofía", "Palmira", "1998-07-14", "F", "2017-08-12");
+INSERT INTO student (first_names, last_names, birth_date, sex, enrollment_date)
+  VALUES ("Pedro", "Escalante", "1998-09-12", "M", "2017-08-12");
+INSERT INTO student (first_names, last_names, birth_date, sex, enrollment_date)
+  VALUES ("Eduardo", "Montalvo", "1999-03-11", "M", "2017-08-12");
