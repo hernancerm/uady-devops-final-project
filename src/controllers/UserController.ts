c115bb61bc3205212e449b35d7dc9b2f6dd0bb74
Axel Anaya <axel.ana25@gmail.com>
2021-10-10 12:48:15 -0500

    Added getAll and post in controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,32 @@
 import { User } from "../entities/User";
 import { UserRepository } from "../repositories/UserRepository";
+import bcrypt from "bcrypt";
 
 import { Request, Response } from "express";
-import { Repository } from "typeorm";
 
-export const StudentController = (userRepository: UserRepository) => {};
+export const UserController = (userRepository: UserRepository) => {
+  const getAll = async (req: Request, res: Response): Promise<Response> => {
+    const users = await userRepository.find();
+    return res.status(200).json(users);
+  };
+
+  const post = async (req: Request, res: Response): Promise<Response> => {
+    console.log("body: ", req.body);
+    const providedUser = Object.assign(new User(), req.body);
+    const salt = await bcrypt.genSalt(10);
+    // now we set user password to hashed password
+    const hashedPassword = await bcrypt.hash(providedUser.password, salt);
+
+    try {
+      const { id } = await userRepository.save({
+        username: providedUser.username,
+        password: hashedPassword,
+      });
+      return res.status(201).json({ userId: id });
+    } catch (error) {
+      return res.status(500).json({ error: "Unexpected DB error" });
+    }
+  };
+
+  return { getAll, post };
+};
