e167859edf343bed8cab9a5eaa91f586725d4a15
Axel Anaya <axel.ana25@gmail.com>
2021-10-10 12:51:46 -0500

    added user router

---
+++ src/routers/impl/UserRouter.ts
@@ -0,0 +1,16 @@
+import { UserController } from "../../controllers/UserController";
+import { UserRepository } from "../../repositories/UserRepository";
+import { RouterAssembler } from "../RouterAssembler";
+
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+
+export const UserRouter = (): RouterAssembler => {
+  const userController = UserController(getCustomRepository(UserRepository));
+
+  const router = Router();
+  router.route("/users").get(userController.getAll);
+  router.route("/auth/signup").post(userController.post);
+
+  return { getAssembledRouter: () => router };
+};
