6c247c601cd57247845b9629682ab0caeac42fdb
Axel Anaya <axel.ana25@gmail.com>
2021-10-10 12:52:16 -0500

    implemented user routes in /api and added json parser

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,20 @@ import { StudentRouter } from "./routers/impl/StudentRouter";
 import "reflect-metadata";
 import express from "express";
 import { createConnection } from "typeorm";
+import { UserRouter } from "./routers/impl/UserRouter";
 
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
 const app = express();
+app.use(express.json());
 
 createConnection().then(() => {
-  app.use("/api", StudentRouter().getAssembledRouter());
+  app.use(
+    "/api",
+    StudentRouter().getAssembledRouter(),
+    UserRouter().getAssembledRouter()
+  );
 });
 
 app.listen(PORT, HOST);
