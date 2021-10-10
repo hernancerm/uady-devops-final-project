c4004b4eeafd5d10c315068d35be050dd75738c3
eyderacm <eyderacm@gmail.com>
2021-10-10 14:30:42 -0500

    feat: add router for course CRUD

--- src/routers/impl/CourseRouter.ts
+++ src/routers/impl/CourseRouter.ts
@@ -6,12 +6,22 @@ import { Router } from "express";
 import { getCustomRepository } from "typeorm";
 
 export const CourseRouter = (): RouterAssembler => {
-  const studentController = CourseController(
+  const courseController = CourseController(
     getCustomRepository(CourseRepository)
   );
 
+  const BASE_PATH = "/courses";
+
   const router = Router();
-  router.route("/courses").get(studentController.getAll);
+  router
+    .route(BASE_PATH)
+    .get(courseController.getAll)
+    .post(courseController.create);
+  router
+    .route(`${BASE_PATH}/:courseId`)
+    .get(courseController.getById)
+    .put(courseController.update)
+    .delete(courseController.deleteById);
 
   return { getAssembledRouter: () => router };
 };
