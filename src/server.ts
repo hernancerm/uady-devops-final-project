5f7a3620b87a4e85adc138890acfcfe5a90d12ae
eyderacm <eyderacm@gmail.com>
2021-10-11 13:22:37 -0500

    fix: course router

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { createConnection } from "typeorm";
 import { UserRouter } from "./routers/impl/UserRouter";
 import { AuthRouter } from "./routers/impl/AuthRouter";
 import { AuthMiddleware } from "./middlewares/AuthMiddleware";
+import { CourseRouter } from "./routers/impl/CourseRouter";
 
 const PORT = 8080;
 const HOST = "0.0.0.0";
@@ -17,6 +18,7 @@ createConnection().then(() => {
   app.use("/api", AuthRouter().getAssembledRouter());
   app.use("/api", authenticateJWT, StudentRouter().getAssembledRouter());
   app.use("/api", authenticateJWT, UserRouter().getAssembledRouter());
+  app.use("/api", authenticateJWT, CourseRouter().getAssembledRouter());
 });
 
 app.listen(PORT, HOST);
