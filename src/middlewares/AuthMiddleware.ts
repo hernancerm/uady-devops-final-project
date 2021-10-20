c4f66a843e0198e350f16c89d66bef3a5a055450
Axel Anaya <axel.ana25@gmail.com>
2021-10-20 17:48:47 -0500

    Logger added to auth middleware

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -1,30 +1,42 @@
 import { Request, Response } from "express";
 import { User } from "../entities/User";
-import jwt from "jsonwebtoken"; 
-const accessTokenSecret = 'youraccesstokensecret';
+import jwt from "jsonwebtoken";
+import { createLogger } from "../loggers/logger";
+const accessTokenSecret = "youraccesstokensecret";
+
+const LOGGER = createLogger(__filename);
 
 export const AuthHelper = () => {
-    const getToken = (username: string): string => {
-        return jwt.sign({ username: username}, accessTokenSecret)
-    };
-    return {getToken};
-}
+  const getToken = (username: string): string => {
+    LOGGER.debug(
+      `Method getToken called  - params: ${JSON.stringify({ username })}`
+    );
+
+    return jwt.sign({ username: username }, accessTokenSecret);
+  };
+  return { getToken };
+};
 
 export const AuthMiddleware = () => {
-    const authenticateJWT = async (req: Request, res: Response, next: () => void) => {
-        const authHeader = req.headers.authorization;
-        if (authHeader) {
-            const token = authHeader.split(' ')[1];
-            jwt.verify(token, accessTokenSecret, (err) => {
-                if (err) {
-                    return res.sendStatus(403);
-                }
-                next();
-            });
-        } else {
-            res.sendStatus(401);
+  const authenticateJWT = async (
+    req: Request,
+    res: Response,
+    next: () => void
+  ) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, accessTokenSecret, (err) => {
+        if (err) {
+          LOGGER.warn("Unauthorized call");
+          return res.sendStatus(403);
         }
-    };
-    return {authenticateJWT};
+        next();
+      });
+    } else {
+      LOGGER.warn("Unauthorized call");
+      res.sendStatus(401);
+    }
+  };
+  return { authenticateJWT };
 };
-  
